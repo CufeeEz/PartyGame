e073e9908df5005995aa1f8c56bfc43fbf2274ce
Daniil <92633278+CufeeEz@users.noreply.github.com>
2023-10-15 19:16:33 +0300

    commit

--- src/main/java/my/cufee/partygame/Games/GoldRush/GoldRushLocation.java
+++ src/main/java/my/cufee/partygame/Games/GoldRush/GoldRushLocation.java
@@ -1,8 +1,15 @@
 package my.cufee.partygame.Games.GoldRush;
 
+import com.sk89q.worldedit.WorldEdit;
 import my.cufee.partygame.MainLocation.SpawnLocation;
 import org.bukkit.Location;
 
 public class GoldRushLocation {
     static Location GRLocationSpawn = new Location(SpawnLocation.getWorld(), -53.5, 63.0, -55.5);
+    static Location[] goldBlocks = new Location[50];
+    public static Location[] setLocGoldBlocks(){
+        goldBlocks[0] = new Location(SpawnLocation.world, 0.0,0.0,0.0);
+        return goldBlocks;
+
+    }
 }
--- src/main/java/my/cufee/partygame/Games/GoldRush/GoldRushStartGame.java
+++ src/main/java/my/cufee/partygame/Games/GoldRush/GoldRushStartGame.java
@@ -1,9 +1,18 @@
 package my.cufee.partygame.Games.GoldRush;
 
+
+import my.cufee.partygame.MainLocation.SpawnLocation;
 import my.cufee.partygame.Util.TeleportPlayers;
+import org.bukkit.Location;
+
 
 public class GoldRushStartGame {
+    static Location l1 = new Location(SpawnLocation.world, -41, 73,-42);
+    static Location l2 = new Location(SpawnLocation.world, -59, 80,-28);
     public static void grStart(){
         TeleportPlayers.teleportInOneLoc(GoldRushLocation.GRLocationSpawn);
+
     }
 }
+
+
