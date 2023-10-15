2f2b89915030ce6693ed746918149d2e7741e0dc
inwar <inw.kirill@gmail.com>
2023-10-15 15:16:53 +0300

    Create game

--- src/main/java/my/cufee/partygame/Games/DigOrDie/DigOrDieGame.java
+++ src/main/java/my/cufee/partygame/Games/DigOrDie/DigOrDieGame.java
@@ -1,4 +1,21 @@
 package my.cufee.partygame.Games.DigOrDie;
 
+import my.cufee.partygame.Games.PlayersArray;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockSpreadEvent;
+
+import javax.xml.stream.Location;
+
+import static my.cufee.partygame.Games.DigOrDie.DigOrDieLocation.locSpawnDigOrDie;
+import static my.cufee.partygame.Games.PlayersArray.*;
+
 public class DigOrDieGame {
+    public static void startDigOrDie(){
+        for (int i = 0; i == playersOnGame.length; i++)
+        {
+            Player teleportedPlayer = playersOnGame[i];
+            teleportedPlayer.teleport(locSpawnDigOrDie[i]);
+        }
+    }
+
 }
--- src/main/java/my/cufee/partygame/Games/DigOrDie/DigOrDieLocation.java
+++ src/main/java/my/cufee/partygame/Games/DigOrDie/DigOrDieLocation.java
@@ -6,7 +6,20 @@ import org.bukkit.Location;
 import org.bukkit.World;
 
 public class DigOrDieLocation {
-    Location[] locationSpawnDigOrDie = new Location[8];
-    World world = Bukkit.getWorld("world");
-    locationSpawnDigOrDie[0] = new Location(world, 0.0, 0.0, 0.0)
+    public static Location[] locSpawnDigOrDie = new Location[8];
+    public static Location[] setLocation(){
+        locSpawnDigOrDie[0] = new Location(SpawnLocation.getWorld(), -40.5, 62.5, 33.5);
+        locSpawnDigOrDie[1] = new Location(SpawnLocation.getWorld(), -30.5, 62.5, 37.5);
+        locSpawnDigOrDie[2] = new Location(SpawnLocation.getWorld(), -26.5, 62.5, 47.5);
+        locSpawnDigOrDie[3] = new Location(SpawnLocation.getWorld(), -30.5, 62.5, 57.5);
+        locSpawnDigOrDie[4] = new Location(SpawnLocation.getWorld(), -40.5, 62.5, 61.5);
+        locSpawnDigOrDie[5] = new Location(SpawnLocation.getWorld(), -50.5, 62.5, 57.5);
+        locSpawnDigOrDie[6] = new Location(SpawnLocation.getWorld(), -54.5, 62.5, 47.5);
+        locSpawnDigOrDie[7] = new Location(SpawnLocation.getWorld(), -50.5, 62.5, 37.5);
+
+
+
+        return locSpawnDigOrDie;
+    }
+
 }
